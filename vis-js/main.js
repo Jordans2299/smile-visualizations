f5310f1a01df1e675dec428a269288643176f4e5
Jordan Stone <jordans2299@gmail.com>
2020-11-23 22:16:34 -0600

    suicideRateExplore

--- vis-js/main.js
+++ vis-js/main.js
@@ -6,6 +6,7 @@ var overviewMapData=[];
 var happyOverTimeData=[];
 var compareDataGDP=[];
 var longeVityData=[];
+var suicideData = [];
 
 // Variable for the visualization instance
 // Start application by loading the data
@@ -14,20 +15,23 @@ loadData();
 
 function loadData() {
     
-var files = ["./../data/world-happiness-report-2015.csv", "./../data/Life Expectancy Data.csv"];
+var files = ["./../data/world-happiness-report-2015.csv", "./../data/Life Expectancy Data.csv","../data/suicideStats1985-2016.csv"];
 var promises = [];
 
 files.forEach(function(url) {
     promises.push(d3.csv(url))
 });
 Promise.all(promises).then(function(values) {
+            
             allDataGDP = values[0];
             longeVityData=values[1].filter(country=>country.Year=="2015")
-            console.log(longeVityData)
+            //console.log(longeVityData)
             happyDataGDP=values[0].slice(0, 15)
             sadDataGDP=values[0].slice(140)
             compareDataGDP= happyDataGDP.concat(sadDataGDP)
             overviewMapData=values[0];
+            suicideData = values[2];
+            //console.log(allDataGDP)
             createVis();
         });
 }
@@ -45,12 +49,16 @@ function createVis() {
     else if (selectionCompare=="life-expectancy"){
         chart = new LifeExpectancyChart("chart-compare", compareDataGDP, longeVityData)
     }
+
    
     if (selectionAll=="GDP"){
         chart = new WorldMapGDP("chart", allDataGDP)
     }
     else if (selectionAll=="life-expectancy"){
         chart = new LifeExpectancyChart("chart", allDataGDP, longeVityData)
+    }
+    else if(selectionAll=="suicide-rate"){
+        chart = new SuicideRateChart("chart", allDataGDP, suicideData)
     }
    
 
