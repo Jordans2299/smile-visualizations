e268773ff453381b168751220f93eedce1518984
Julia Smith <42749098+smithj11@users.noreply.github.com>
2020-12-06 21:25:34 -0600

    Update main.js

--- vis-js/main.js
+++ vis-js/main.js
@@ -68,14 +68,6 @@ function createVis() {
     let xLabel = document.getElementById("metrics-explore").selectedOptions[0].attributes[1].nodeValue;
     exploreChart = new LineGraphTemplate("chart", allDataGDP, selectionAll, "Happiness_Score", xLabel, "Happiness Score", "All")
     compareChart = new LineGraphTemplate("chart-compare", compareDataGDP, selectionAll, "Happiness_Score", xLabel, "Happiness Score", "All")
-    if (selectionOfWorldMap == "data-country-or-region") {
-        document.getElementById("world-map-color-coded").innerHTML = "";
-        overviewmap = new OverviewMap("world-map-color-coded", overviewMapData)
-    } else if (selectionOfWorldMap == "data-continent") {
-        document.getElementById("world-map-color-coded").innerHTML = "";
-        overviewmap = new ContinentMap("world-map-color-coded", overviewContinentData)
-    }
-    chart = new LineGraphTemplate("chart", allDataGDP, selectionAll, "Happiness_Score", xLabel, "Happiness Score")
     new CorrelationDisplay("one",allDataGDP,"Economy (GDP per Capita)","Happiness_Score", 500,250);
     new CorrelationDisplay("two",allDataGDP,"Health (Life Expectancy)","Happiness_Score", 500,250);
     new CorrelationDisplay("three",allDataGDP,"Family","Happiness_Score", 500,250);
