73fc599bffaf06c9ab304aaa234ea9ac54569a40
AndyWu1999 <wu.tianqi@outlook.com>
2020-12-11 19:27:50 +0800

    Tweaks

--- vis-js/worldmap.js
+++ vis-js/worldmap.js
@@ -3,7 +3,7 @@ OverviewMap = function(_parentElement, _data) {
     this.parentElement = _parentElement;
     this.data = _data;
     this.width = 1000,
-    this.height = 600;
+        this.height = 600;
     this.initVis();
 }
 
@@ -23,10 +23,10 @@ OverviewMap.prototype.initVis = function() {
 
     vis.wrangleData();
     vis.svg = d3.select("#" + vis.parentElement)
-    .append("svg")
-    .attr("height", vis.height)
-    .attr("width", vis.width)
-    .attr("id", "map-svg")
+        .append("svg")
+        .attr("height", vis.height)
+        .attr("width", vis.width)
+        .attr("id", "map-svg")
 
     /*
         vis.svg.append("g")
@@ -49,6 +49,22 @@ OverviewMap.prototype.wrangleData = function() {
     // Update the visualization
     vis.updateVis();
 
+}
+
+/**
+ * Renders the HTML content for tool tip
+ *
+ * @param tooltip_data information that needs to be populated in the tool tip
+ * @return text HTML content for toop tip
+ */
+OverviewMap.prototype.tooltip_render = function(tooltip_data) {
+    var vis = this;
+    var text = "<ul>";
+    //tooltip_data.forEach(function(row) {
+    text += "<li>" + "<h3>" + " " + "\t\t" + tooltip_data.Country + "</h3>" + "</li>" + "<li>" + vis.xlabel + ":\t\t" + tooltip_data[vis.x] + "</li><li>Region" + ":\t\t" + tooltip_data.Region + "" + "</li>"
+        //});
+
+    return text;
 }
 
 
@@ -82,6 +98,11 @@ OverviewMap.prototype.updateVis = function() {
         .projection(projection);
 
 
+    var div = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+
     d3.json("data/world-countries.json").then(function(world) {
         console.log(vis.data[0])
         vis.svg.append("g")
@@ -101,7 +122,28 @@ OverviewMap.prototype.updateVis = function() {
                 return "#808080";
                 // pass rate value to color function, return color based on domain and range
             })
-            .on("click", function(event, d) {
+            .on("mouseover", function(event, d) {
+
+                div.transition()
+                    .duration(200)
+                    .style("opacity", .8);
+                div.html(function() {
+                        let nameaa = undefined;
+
+                        let countryName = d.properties.name;
+                        var result = vis.data.filter(country => {
+                            return country.Country == countryName
+                        })
+                        if (result.length == 1) {
+                            nameaa = result[0].Happiness_Score;
+                        }
+
+                        return "<h3>" + d.properties.name +
+                            "</h3><br/>" +
+                            "Happiness Score: " + nameaa;
+                    })
+                    .style("left", (event.pageX) + "px")
+                    .style("top", (event.pageY - 18) + "px");
 
                 vis.svg.selectAll("path")
                     .transition(500)
@@ -121,26 +163,32 @@ OverviewMap.prototype.updateVis = function() {
                 d3.select(event.currentTarget)
                     .transition(500)
                     .style("fill", "green");
-                //if(set_of_nations.has(d.properties.name)){
-                //setUpCountries(d.properties.name);
-                //}
-                //console.log(d);
-            }).on("dblclick",
-                function(d) {
-                    vis.svg.selectAll("path")
-                        .transition(500)
-                        .style("fill", function(d) {
-                            let countryName = d.properties.name;
-                            var result = vis.data.filter(country => {
-                                return country.Country == countryName
-                            })
-                            if (result.length == 1) {
-                                return vis.colorScale(result[0].Happiness_Score);
-                            } // get rate value for property matching data ID
-                            return "#808080";
-                            // pass rate value to color function, return color based on domain and range
+
+
+
+
+            })
+            .on("mouseout", function(d) {
+                vis.svg.selectAll("path")
+                    .transition(500)
+                    .style("fill", function(d) {
+                        let countryName = d.properties.name;
+                        var result = vis.data.filter(country => {
+                            return country.Country == countryName
                         })
-                });
+                        if (result.length == 1) {
+                            return vis.colorScale(result[0].Happiness_Score);
+                        } // get rate value for property matching data ID
+                        return "#808080";
+                        // pass rate value to color function, return color based on domain and range
+                    })
+
+                div.transition()
+                    .duration(300)
+                    .style("opacity", 0);
+
+
+            });
 
     });
 
